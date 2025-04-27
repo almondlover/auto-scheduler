c64518082ef0cdaa5475089fd5abe95c9f704c7d
Anton Kostov <tonigreat@abv.bg>
2025-04-27 11:45:18 +0300

    updated dbcontext class with tables

--- AutoSchedulerBE/AutoScheduler.DataAccess/SchedulerContext.cs
+++ AutoSchedulerBE/AutoScheduler.DataAccess/SchedulerContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoScheduler.Domain.Entities.Activities;
+using AutoScheduler.Domain.Entities.MemberGroups;
+using AutoScheduler.Domain.Entities.Timesheets;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,16 @@ using System.Threading.Tasks;
 
 namespace AutoScheduler.DataAccess
 {
-    internal class SchedulerContext:DbContext
+    public class SchedulerContext:DbContext
     {
+        public required DbSet<Timesheet> Timesheets;
+        public required DbSet<Timeslot> Timeslots;
+        public required DbSet<Group> Groups;
+        public required DbSet<Member> Members;
+        public required DbSet<Organization> Organizations;
+        public required DbSet<Activity> Activities;
+        public required DbSet<ActivityRequirements> ActivityRequirements;
+        public required DbSet<Hall> Halls;
+        public required DbSet<HallType> HallTypes;
     }
 }
