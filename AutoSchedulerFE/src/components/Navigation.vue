793f0c82ae5f8ab3a1e36231b91d3db4218a8029
Anton Kostov <tonigreat@abv.bg>
2025-04-28 08:19:44 +0300

    setup base layout&rm init project files

---
+++ AutoSchedulerFE/src/components/Navigation.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
