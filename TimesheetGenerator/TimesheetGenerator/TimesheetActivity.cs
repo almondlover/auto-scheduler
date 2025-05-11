5dcec70cac1e737e3979408f2d34e0971d11e5c5
Anton Kostov <tonigreat@abv.bg>
2025-05-12 00:53:02 +0300

    added solution for generation module&simple initial algorithm

---
+++ TimesheetGenerator/TimesheetGenerator/TimesheetActivity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesheetGenerator
+{
+    internal class TimesheetActivity
+    {
+        public int SlotCount { get; }
+        public bool[] PresenterAvailability { get; }
+        public bool[] HallAvailability { get; }
+        public bool[] PotentialSlots { get; }
+
+    }
+}
---
+++ TimesheetGenerator/TimesheetGenerator/TimesheetGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace TimesheetGenerator
+{
+	public class TimesheetGenerator
+	{
+		private int[] _vacantSlots;
+		private TimesheetActivity[] _activities;
+		public List<int[]> generated;
+		public TimesheetGenerator(int totalSlots)
+		{
+			_vacantSlots = new int[totalSlots];
+		}
+		public void ReserveSlots(int currentActivityIdx, int[] vacantSlots)
+		{
+			if (currentActivityIdx == _activities.Length)
+			{
+				for (int i = 0; i < _vacantSlots.Length; i++)
+					if (!_vacantSlots.Contains(i)) return;
+				generated.Add(vacantSlots);
+				return;
+			}
+
+			for (int i=0; i< vacantSlots.Length; i++)
+			{
+				int[] newVacantSlots;
+                if (vacantSlots[i] == 0 && _activities[currentActivityIdx].PotentialSlots[i])
+				{
+					newVacantSlots = (int[])vacantSlots.Clone();
+					int j;
+					for (j=i; _activities[currentActivityIdx].PotentialSlots[j]&&j-1< _activities[currentActivityIdx].SlotCount; j++)
+					{
+						if (vacantSlots[j] == 0)
+						{
+							i = j;
+							break;
+						}
+						//put activity in the free slots
+						newVacantSlots[j] = currentActivityIdx;
+                    }
+					//stupid check
+                    if (i!=j) ReserveSlots(currentActivityIdx++, newVacantSlots);
+				}
+			}
+		}
+	}
+}
