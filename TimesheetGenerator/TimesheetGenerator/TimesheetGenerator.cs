7673c4766674b460a046dc544eb26f3e00908766
Anton Kostov <tonigreat@abv.bg>
2025-05-26 02:12:50 +0300

    fix generation bugs-now successfully generates reserved slot combinations

--- TimesheetGenerator/TimesheetGenerator/TimesheetGenerator.cs
+++ TimesheetGenerator/TimesheetGenerator/TimesheetGenerator.cs
@@ -9,6 +9,8 @@ namespace TimesheetGenerator
 		public List<List<int[]>> generated;
 		private bool[][] _presentersAvailability;
         private bool[][] _hallsAvailability;
+		private int[] _presenterMapping;
+		private int[] _hallMapping;
         public TimesheetGenerator(int totalSlots, bool[][] presentersAvailability, bool[][] hallsAvailability)
 		{
 			_vacantSlots = new int[totalSlots];
@@ -18,7 +20,9 @@ namespace TimesheetGenerator
 		public void InitActivities(int[] durations, int[] presenterMapping, int[] hallMapping)
 		{
 			_activities = new TimesheetActivity[durations.Length];
-			for (int i=0; i < durations.Length; i++)
+			_presenterMapping = presenterMapping;
+			_hallMapping = hallMapping;
+            for (int i=0; i < durations.Length; i++)
 			{
 				_activities[i] = new TimesheetActivity();
                 _activities[i].SlotCount = durations[i];
@@ -39,64 +43,87 @@ namespace TimesheetGenerator
 		{
 			if (currentActivityIdx == _activities.Length)
 			{
-				for (int i = 0; i < _vacantSlots.Length; i++)
-					if (!_vacantSlots.Contains(i)) return;
+				//for (int i = 0; i < _vacantSlots.Length; i++)
+				//	if (!_vacantSlots.Contains(i)) return;
 				if (generated.Count<10) generated.Add(reservedSlots);
 				return;
 			}
 
             activities[currentActivityIdx].UpdateAvailability();
 
-            for (int i=0; i< activities[currentActivityIdx].PotentialSlots.Count && i < reservedSlots.Count-1; i++)
+			int reservedIdx = 0;
+            for (int i=0; i < activities[currentActivityIdx].PotentialSlots.Count; i++)
 			{
 				int[] newVacantSlots;
 
 				//reserved slots need to be sorted by slot index
-                if (reservedSlots[i][0] + activities[reservedSlots[i][1]].SlotCount < activities[currentActivityIdx].PotentialSlots[i][0]
-					/*&& _activities[currentActivityIdx].PotentialSlots[i][0] + _activities[currentActivityIdx] < reservedSlots[i+1][0]*/)
+				//while (reservedIdx < reservedSlots.Count
+    //                    && activities[currentActivityIdx].PotentialSlots[i][0] > reservedSlots[reservedIdx][0] + activities[reservedSlots[reservedIdx][1]].SlotCount)
+				//	reservedIdx++;
+
+				//iterate over the current available space for the activity
+				int j = activities[currentActivityIdx].PotentialSlots[i][0];
+				while (reservedIdx < reservedSlots.Count
+						&& j > reservedSlots[reservedIdx][0]
+						&& j + activities[currentActivityIdx].SlotCount < activities[currentActivityIdx].PotentialSlots[i][0] + activities[currentActivityIdx].PotentialSlots[i][1])
 				{
-					//iterate over the current available space fo the activity
-					int j = activities[currentActivityIdx].PotentialSlots[i][0];
-					while (j + activities[currentActivityIdx].SlotCount < reservedSlots[i + 1][0]
-							&& j + activities[currentActivityIdx].SlotCount < activities[currentActivityIdx].PotentialSlots[i][0] + activities[currentActivityIdx].PotentialSlots[i][1])
+					//check next reserved slot
+					j = Math.Max(j, reservedSlots[reservedIdx][0] + activities[reservedSlots[reservedIdx][1]].SlotCount);
+					reservedIdx++;
+					continue;
+				}
+
+				while (j + activities[currentActivityIdx].SlotCount < activities[currentActivityIdx].PotentialSlots[i][0] + activities[currentActivityIdx].PotentialSlots[i][1])
+				{
+					if (reservedIdx >= reservedSlots.Count
+						|| j + activities[currentActivityIdx].SlotCount < reservedSlots[reservedIdx][0])
 					{
 						//clone collections
-						List<int[]> newReservedSlots = (List<int[]>)reservedSlots.Select(slot => slot.Clone());
-                        newReservedSlots.Add(new int[] { j, currentActivityIdx });
+						List<int[]> newReservedSlots = reservedSlots.Select(slot => (int[])slot.Clone()).ToList();
+						newReservedSlots.Add(new int[] { j, currentActivityIdx });
 
-                        TimesheetActivity[] newActivities = activities.Select(a => {
-							var newAct = new TimesheetActivity()
-								{
-									PotentialSlots = a.PotentialSlots,
-									SlotCount = a.SlotCount
-								};
-							return newAct;
-							}).ToArray();
 
 						//link avail. to activity
-                        bool[][] newHallsAvailability = hallsAvailability.Select(h => {
-								var newAvail = (bool[])h.Clone();
-								if (h == activities[currentActivityIdx].HallAvailability)
-									newActivities[currentActivityIdx].HallAvailability = newAvail;
-								return newAvail;
-                            }).ToArray();
-                        bool[][] newPresentersAvailability = presentersAvailability.Select(h => {
-                            var newAvail = (bool[])h.Clone();
-                            if (h == activities[currentActivityIdx].PresenterAvailability)
-                                newActivities[currentActivityIdx].PresenterAvailability = newAvail;
-                            return newAvail;
+						bool[][] newHallsAvailability = hallsAvailability.Select(h =>
+						{
+							var newAvail = (bool[])h.Clone();
+							return newAvail;
+						}).ToArray();
+						bool[][] newPresentersAvailability = presentersAvailability.Select(h =>
+						{
+							var newAvail = (bool[])h.Clone();
+							return newAvail;
+						}).ToArray();
+
+                        TimesheetActivity[] newActivities = activities.Select((a, i) =>
+                        {
+                            var newAct = new TimesheetActivity()
+                            {
+                                PotentialSlots = a.PotentialSlots,
+                                SlotCount = a.SlotCount,
+                                //should probably be init elsewhere
+                                HallAvailability = newHallsAvailability[_hallMapping[i]],
+                                PresenterAvailability = newPresentersAvailability[_presenterMapping[i]]
+                            };
+                            return newAct;
                         }).ToArray();
 
-						//presenter/hall avail. should probably also be refactored or used as a property
-						for (int k = j; k < j + _activities[currentActivityIdx].SlotCount; k++)
+                        //presenter/hall avail. should probably also be refactored or used as a property
+                        for (int k = j; k < j + _activities[currentActivityIdx].SlotCount; k++)
 						{
-							newActivities[currentActivityIdx].HallAvailability[k] = false;
-                            newActivities[currentActivityIdx].PresenterAvailability[k] = false;
-                        }
-						ReserveSlots(currentActivityIdx++, newReservedSlots, newActivities, newPresentersAvailability, newHallsAvailability);
+							newActivities[currentActivityIdx].HallAvailability[k] = true;
+							newActivities[currentActivityIdx].PresenterAvailability[k] = true;
+						}
+						ReserveSlots(currentActivityIdx+1, newReservedSlots, newActivities, newPresentersAvailability, newHallsAvailability);
 						j++;
-                    }
-				}
+					}
+					else
+					{
+						//set index after current reserved slot
+						j = reservedSlots[reservedIdx][0] + activities[reservedSlots[reservedIdx][1]].SlotCount;
+						reservedIdx++;
+					}
+                }
 			}
 		}
 	}
