1a3e46c3548901ce0ef598598fa6a040fefc816f
Anton Kostov <tonigreat@abv.bg>
2026-05-24 18:26:16 +0300

    added subtypes for activitytypes migration

--- AutoSchedulerBE/AutoScheduler.Domain/Entities/Activities/ActivityType.cs
+++ AutoSchedulerBE/AutoScheduler.Domain/Entities/Activities/ActivityType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace AutoScheduler.Domain.Entities.Activities
     public class ActivityType
     {
         public int Id { get; set; }
+        public int? BaseTypeId { get; set; }
         public required string Title { get; set; }
         public string? Description { get; set; }
+        [ForeignKey("BaseTypeId")]
+        public IList<ActivityType>? SubTypes { get; set; }
     }
 }
