fa991c582be626e8c17c4d43d0a5a5359143041b
Anton Kostov <tonigreat@abv.bg>
2026-05-31 23:46:07 +0300

    add fe utility func for calculating timeranges overlap

--- AutoSchedulerFE/src/utils/timediff.ts
+++ AutoSchedulerFE/src/utils/timediff.ts
@@ -7,3 +7,16 @@ export function timeDiffInMinutes(time1:string, time2:string)
 
     return msecDiff/60000;
 };
+export function timeRangesOverlap (startTime1:string, endTime1:string, startTime2:string, endTime2:string)
+{
+    const startTime1AsTime = new Date(`"2000/01/01"${startTime1}`).getTime();
+    const endTime1AsTime = new Date(`"2000/01/01"${endTime1}`).getTime();
+    const startTime2AsTime = new Date(`"2000/01/01"${startTime2}`).getTime();
+    const endTime2AsTime = new Date(`"2000/01/01"${endTime2}`).getTime();
+
+    if (!((startTime1AsTime < endTime2AsTime) && (endTime1AsTime > startTime2AsTime)))
+        return null;
+    
+    return { startTime: startTime1AsTime > startTime2AsTime ? startTime1 : startTime2,
+         endTime: endTime1AsTime < endTime2AsTime ? endTime1 : endTime2 };
+}
