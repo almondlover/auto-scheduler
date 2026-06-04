846f5588753534243ca40e703f5ac14113420de1
Anton Kostov <tonigreat@abv.bg>
2026-06-04 09:04:48 +0300

    add user seeding for dev testing, update swagger config

--- AutoSchedulerBE/AutoScheduler.DataAccess/Seeders/IdentitySeeder.cs
+++ AutoSchedulerBE/AutoScheduler.DataAccess/Seeders/IdentitySeeder.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoScheduler.DataAccess.Seeders.Models;
+using AutoScheduler.Domain.Entities.Users;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AutoScheduler.DataAccess.Seeders
@@ -6,6 +8,26 @@ namespace AutoScheduler.DataAccess.Seeders
     public static class IdentitySeeder
     {
         static readonly string[] roleNames = ["Admin", "ResourceManager", "ScheduleManager"];
+        static readonly UserSeed[] testUsers = [
+            new UserSeed {
+                Username = "TestAdmin",
+                Password = "TestPassword123!",
+                Email = "testadmin@mail.com",
+                Role = "Admin"
+            },
+            new UserSeed {
+                Username = "TestManager",
+                Password = "TestPassword123!",
+                Email = "testmng@mail.com",
+                Role = "ResourceManager"
+            },
+            new UserSeed {
+                Username = "TestScheduler",
+                Password = "TestPassword123!",
+                Email = "testsched@mail.com",
+                Role = "ScheduleManager"
+            },
+        ];
         public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
@@ -14,6 +36,25 @@ namespace AutoScheduler.DataAccess.Seeders
             {
                 if (!await roleManager.RoleExistsAsync(roleName))
                     await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+        public static async Task SeedTestUsersAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            foreach (var user in testUsers)
+            {
+                if (await userManager.FindByEmailAsync(user.Email) == null)
+                {
+                    var newUser = new User
+                    {
+                        UserName = user.Username,
+                        Email = user.Email
+                    };
+                    
+                    await userManager.CreateAsync(newUser, user.Password);
+                    await userManager.AddToRoleAsync(newUser, user.Role);
+                }
             }
         }
     }
