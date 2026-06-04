1d0321c537a5380639ce4db2fd7de1b3aa228db5
Anton Kostov <tonigreat@abv.bg>
2026-06-04 18:20:38 +0300

    migration - added missing dbset fpor activitytypes

---
+++ AutoSchedulerBE/AutoScheduler.DataAccess/Migrations/20260604151919_ActivityTypeSetAdd.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoScheduler.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivityTypeSetAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityType_ActivityTypeId",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActivityType_ActivityType_BaseTypeId",
+                table: "ActivityType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityType",
+                table: "ActivityType");
+
+            migrationBuilder.RenameTable(
+                name: "ActivityType",
+                newName: "ActivityTypes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ActivityType_BaseTypeId",
+                table: "ActivityTypes",
+                newName: "IX_ActivityTypes_BaseTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityTypes",
+                table: "ActivityTypes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityTypes_ActivityTypeId",
+                table: "Activities",
+                column: "ActivityTypeId",
+                principalTable: "ActivityTypes",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActivityTypes_ActivityTypes_BaseTypeId",
+                table: "ActivityTypes",
+                column: "BaseTypeId",
+                principalTable: "ActivityTypes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityTypes_ActivityTypeId",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActivityTypes_ActivityTypes_BaseTypeId",
+                table: "ActivityTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityTypes",
+                table: "ActivityTypes");
+
+            migrationBuilder.RenameTable(
+                name: "ActivityTypes",
+                newName: "ActivityType");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ActivityTypes_BaseTypeId",
+                table: "ActivityType",
+                newName: "IX_ActivityType_BaseTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityType",
+                table: "ActivityType",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityType_ActivityTypeId",
+                table: "Activities",
+                column: "ActivityTypeId",
+                principalTable: "ActivityType",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActivityType_ActivityType_BaseTypeId",
+                table: "ActivityType",
+                column: "BaseTypeId",
+                principalTable: "ActivityType",
+                principalColumn: "Id");
+        }
+    }
+}
